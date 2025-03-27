ecef8cdfed5b43a11f4164145bf842128a6647e2
Vojtěch Rada <94050097+VojtechRada@users.noreply.github.com>
2025-03-27 09:06:10 +0100

    Done
    všechny testy proběhly správně

---
+++ tests/test_divide.py
@@ -0,0 +1,12 @@
+import pytest
+from src import calculator
+
+def test_devide():
+    with pytest.raises(ValueError) as exc:
+        calculator.divide(5, 0)
+    assert str(exc.value) == "Cannot divide by zero!"
+
+def test_devide():
+    with pytest.raises(ValueError) as exc:
+        calculator.divide(5, 0)
+    assert str(exc.value) == "Cannot divide by zero!"
